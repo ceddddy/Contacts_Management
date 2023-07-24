8282e802ada84d2d9c1181a1d73c6df164780145
cedy <cedy@DESKTOP-749ON5S>
2023-07-23 17:17:55 -0700

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
